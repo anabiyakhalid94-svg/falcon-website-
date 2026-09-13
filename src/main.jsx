ae3f89bf2829a958e02b302c8b11f273007ee210
anabiyakhalid94-svg <anabiyakhalid94@gmail.com>
2026-09-13 04:15:12 -0700

    Fix routing for root hosting and GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './styles.css';
 
+const basename = window.location.pathname.startsWith('/falcon-website-')
+  ? '/falcon-website-'
+  : '/';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
